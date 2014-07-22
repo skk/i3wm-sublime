3b6267445610327f80ab16d842f7d02e7daef656
Steven Knight <steven@knight.cx>
2014-07-22 16:46:11 -0400

    Removed rsync to sublime-text-2

--- build.sh
+++ build.sh
@@ -15,5 +15,4 @@
 
 yaml-to-plist.rb Syntax/i3wm_config.YAML-tmLanguage > Syntax/i3wm_config.tmLanguage
 
-rsync -av ~/Projects/i3wm-sublime/ ~/.config/sublime-text-2/Packages/i3wm-sublime
 rsync -av ~/Projects/i3wm-sublime/ ~/.config/sublime-text-3/Packages/i3wm-sublime
